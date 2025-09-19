653a1b979de034e912a20dac905213e085c13923
Peccer <paco.deboer@gmail.com>
2025-09-19 16:13:49 +0200

    Add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.ts',
+};
